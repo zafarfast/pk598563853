d19ef97ad6edf281305ae7a1cc0349a081d5d0cf
zafarfast <zafar_fast@hotmail.com>
2023-02-20 02:00:43 +1100

    added keys and diagram

--- lib/db/schema.sql
+++ lib/db/schema.sql
@@ -3,22 +3,36 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
+CREATE TABLE department (
+  id INT PRIMARY KEY NOT NULL,
+  dept_name VARCHAR(30) NOT NULL
+);
+
+
+
+CREATE TABLE role1 (
+    id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(30),
+    salary INT,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+    );
+
 CREATE TABLE employee (
-  id INT ,
+  id INT PRIMARY KEY NOT NULL,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role1(id)
+  ON DELETE SET NULL,
+
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
 );
 
-CREATE TABLE role1 (
-    id INT,
-    title VARCHAR(30),
-    salary INT,
-    department_id INT
-    );
 
-CREATE TABLE department (
-  id INT,
-  dept_name VARCHAR(30)
-);
